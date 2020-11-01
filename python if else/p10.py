ed2b33a1124b0b9a1b519d30a096ef97b5471e89
Anshir08 <69457505+Anshir08@users.noreply.github.com>
2020-11-01 03:31:21 -0800

    Update p10.py
    Hey there take this one too...

--- python if else/p10.py
+++ python if else/p10.py
@@ -1,11 +1,9 @@
-import math
-a=float(input("Enter the coefficient a"))
-b=float(input("Enter the coefficient b"))
-c=float(input("Enter the coefficient c"))
-d=b*b-4*a*c;
+a, b, c = float(input("Enter the coefficient a")), float(input("Enter the coefficient b")), float(input("Enter the coefficient c"))
+
+d = (b**2) - (4*a*c)
 if d>0:
-    r1=(-b+math.sqrt(d))/(2*a)
-    r2 =(-b-math.sqrt(d))/(2*a)
+    r1=(-b + d**(1/2))/(2*a)
+    r2 =(-b - d**(1/2))/(2*a)
     print("Roots are real and unequal",r1,"and",r2)
 elif d==0:
     r1=-b/(2*a)
